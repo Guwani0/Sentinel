eff4726782664e9d53edb87ede4b4264a58bd3ad
Amath Perera <jinuka.markperera@gmail.com>
2025-09-26 20:18:58 +0530

    fix error cors

--- backend/app.js
+++ backend/app.js
@@ -3,15 +3,27 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const authRoutes = require("./Routes/authRoutes");
 const path = require("path"); //nethupul
-//const path = require('path'); //nomasha
 const bodyParser = require("body-parser"); //nethupul
 const incidentRoutes = require("./Routes/incidentRoutes");
 
 dotenv.config();
 const app = express();
 
+
 // Middleware
 app.use(express.json());
+
+//Nomasha -----
+// Simple CORS middleware (allow all origins for dev)
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    if (req.method === 'OPTIONS') {
+        res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE');
+        return res.status(200).json({});
+    }
+    next();
+});
 
 // Routes
 app.use("/api/auth", authRoutes);
@@ -22,17 +34,7 @@ const policyRoutes = require("./Routes/policies");
 app.use("/api/policies", policyRoutes);
 app.use("/api/incidents", incidentRoutes);
 
-//Nomasha -----
-// Simple CORS middleware (allow all origins for dev)
-// app.use((req, res, next) => {
-//     res.header('Access-Control-Allow-Origin', '*');
-//     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-//     if (req.method === 'OPTIONS') {
-//         res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE');
-//         return res.status(200).json({});
-//     }
-//     next();
-// });
+
 
 // Mount API routes
 //const apiRoutes = require(path.join(__dirname, 'Routes', 'GRoutes'));
